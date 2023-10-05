f549b6e1aa7bbfd8555129ec4ab1726229a874d4
ishita240 <ishitapatel2402@gmail.com>
2023-10-05 10:56:31 -0400

    my first branch

--- Ishita 24ConsoleApp1/Program.cs
+++ Ishita 24ConsoleApp1/Program.cs
@@ -10,26 +10,9 @@ namespace Ishita_24ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("\n\nCheck whether a character is alphabet or not and if so, check for case :\n");
-            Console.Write("-----------------------------------------------------------------------------\n");
-            Console.Write("Input a character: ");
-            char ch = (char)Console.Read();
-            if (Char.IsLetter(ch))
-            {
-                if (Char.IsUpper(ch))
-                {
-                    Console.WriteLine("\nThe character is uppercase.\n");
-                }
-                else
-                {
-                    Console.WriteLine("\nThe character is lowercase.\n");
-                }
-            }
-            else
-            {
-                Console.WriteLine("\nThe entered character is not an alphabetic character.\n");
+            
             }
         }
     }
-}
+
     

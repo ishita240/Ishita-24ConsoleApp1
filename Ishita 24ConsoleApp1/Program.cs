989250f38364f12ae0f9605a0bbfb30609fae6ae
ishita240 <ishitapatel2402@gmail.com>
2023-10-07 11:03:41 -0400

    my demo program..

--- Ishita 24ConsoleApp1/Program.cs
+++ Ishita 24ConsoleApp1/Program.cs
@@ -10,26 +10,15 @@ namespace Ishita_24ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("\n\nCheck whether a character is alphabet or not and if so, check for case :\n");
-            Console.Write("-----------------------------------------------------------------------------\n");
-            Console.Write("Input a character: ");
-            char ch = (char)Console.Read();
-            if (Char.IsLetter(ch))
-            {
-                if (Char.IsUpper(ch))
-                {
-                    Console.WriteLine("\nThe character is uppercase.\n");
-                }
-                else
-                {
-                    Console.WriteLine("\nThe character is lowercase.\n");
-                }
-            }
-            else
-            {
-                Console.WriteLine("\nThe entered character is not an alphabetic character.\n");
-            }
+            // create string
+            string str = "C# Programming";
+            Console.WriteLine("string: " + str);
+
+            // get length of str
+            int length = str.Length;
+            Console.WriteLine("Length: " + length);
+
+            Console.ReadLine();
         }
     }
 }
-    
